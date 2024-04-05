683b7b845c60165a8f025bb360fada23188a916c
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2024-04-06 01:55:39 +0700

    fix: fix scrolling bug

--- AR_Math_2/Assets/Script/Carousel.cs
+++ AR_Math_2/Assets/Script/Carousel.cs
@@ -9,6 +9,8 @@ public class Carousel : MonoBehaviour
     public float scroll_pos = 0;
     float[]pos;
     float distance;
+
+    public bool checkMove = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,22 +24,67 @@ public class Carousel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+         if (checkMove) return;
 
-       if (Input.GetMouseButton(0)){
-        scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
-       }
+         if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                //   scroll_pos += touch.deltaPosition.x * Time.deltaTime;
+               scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+            }
+        }
        else {
+        //  scroll_pos = Mathf.Clamp01(scroll_pos);
+        
         for (int i = 0; i< pos.Length; i ++){
             if (scroll_pos < pos[i] + (distance/2) && scroll_pos > pos [i] - (distance/2)){
                 scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
+                 scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+                 break;
+                 
             }
         }
        }
+       
 
        for (int i = 0; i< pos.Length; i ++){
             float targetScale = (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2)) ? 1f : 0.8f;
             transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(targetScale, targetScale), 0.1f);
         }
+    }
+
+    public void MoveLeft(){
+       
+       checkMove = true;
+
+        for (int i = 0; i< pos.Length; i ++){
+            if (i!=0 && scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2)){
+                // scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i-1], 0.1f);
+                  
+                  scrollbar.GetComponent<Scrollbar>().value = pos[i-1];
+                  scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+                 break;
+             
+            }
+        }
+
+        checkMove = false;
+    }
+
+     public void MoveRight(){
+         checkMove = true;
+        for (int i = 0; i< pos.Length; i ++){
+            if (i!=pos.Length-1 && scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2)){
+                // scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i+1], 0.1f);
+                    scrollbar.GetComponent<Scrollbar>().value = pos[i+1];
+                  scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+                 break;
+         
+            }
+        }
+
+         checkMove = false;
     }
 }
